f52957cf3d953c2c2f2d6078cce39a2a4b6f340f
Venkat <venkatramanbehera1@gmail.com>
2022-05-15 13:30:22 +0530

    active custom hooks

---
+++ src/custom/useActive.js
@@ -0,0 +1,32 @@
+import { useEffect, useState, useRef } from "react";
+
+export function useActive(time) {
+  const [active, setActive] = useState(false);
+  const timer = useRef();
+
+  useEffect(() => {
+    const events = ["keypress", "mousemove", "touchmove", "click", "scroll"];
+    const handleEvent = () => {
+      setActive(true);
+      if (timer.current) {
+        window.clearTimeout(timer.current);
+      }
+
+      timer.current = window.setTimeout(() => {
+        setActive(false);
+      }, time);
+    };
+
+    events.forEach((event) => {
+      document.addEventListener(event, handleEvent);
+    });
+
+    return () => {
+      events.forEach((event) => {
+        document.removeEventListener(event, handleEvent);
+      });
+    };
+  }, [time]);
+
+  return active;
+}
